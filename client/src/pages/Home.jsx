21e0baa95489aaa1792db9a55f20ffecc194d2c4
Aman Kushwaha <amankushwaha7284@gmail.com>
2026-01-07 23:21:46 +0530

    Setup Project and layout

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Banner from "../components/Home/Banner"
+import Features from "../components/home/Features"
+import Hero from "../components/home/Hero"
+
+function Home() {
+    return (
+        <div>
+           <Banner/>
+           <Hero/>
+           <Features/>
+        </div>
+    )
+}
+
+export default Home
---
+++ client/src/pages/Preview.jsx
@@ -0,0 +1,9 @@
+function Preview() {
+    return (
+        <div>
+            <h1>Preview page</h1>
+        </div>
+    )
+}
+
+export default Preview
