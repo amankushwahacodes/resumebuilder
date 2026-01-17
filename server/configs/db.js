aa84460eca510c425ee94cc7832c5ed65168feeb
Aman Kushwaha <amankushwaha7284@gmail.com>
2026-01-17 15:58:13 +0530

    Backend setup

---
+++ server/configs/db.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database connected successfully");
+    });
+
+    let mongodbURI = process.env.MONGODB_URI;
+
+    const projectName = "ResumeBuilder";
+
+    if (!mongodbURI) {
+      throw new Error("MONGODB_URI enviornment variable not set");
+    }
+
+    if (mongodbURI.endsWith("/")) {
+      mongodbURI = mongodbURI.slice(0, -1);
+    }
+
+    await mongoose.connect(`${mongodbURI}/${projectName}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDb",error);
+  }
+};
+
+export default connectDB;
